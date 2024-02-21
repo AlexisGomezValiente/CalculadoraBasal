b7e52a35e88788ad5efb412e979bb065a5be570b
AlexisGomez <wilaleva2019@gmail.com>
2024-02-20 21:20:45 -0400

    Agrego la posibilidad de eleccion
    Posibilidad del doctor a elegir cuando el peso es mayor a 30, realizar el calculo y al finalizar si multiplicar por 1500 o 2000

--- index.js
+++ index.js
@@ -26,15 +26,27 @@
 // console.log(mantenimiento + " cc / hr."); //mantenimiento por hora
 // console.log(mm2 + " m+m/2"); //m+m/2
 
-
-
 //!APARTIR DE ACA ES EL SCRIPT PARA EL PROYECTO
 let peso = document.getElementById("peso");
 let calcular = document.getElementById("calcular");
 let resultado1 = document.getElementById("resultados1");
 let resultado2 = document.getElementById("resultados2");
+let mayor30 = document.getElementById("mayor30");
+let op1500 = document.getElementById("1500");
+let op2000 = document.getElementById("2000");
 
-const calculo = () => {
+const calculoDetalles = (peso, liquido) =>{
+  if(peso > 30) mayor30.style = "display: none";
+
+  let total = liquido;
+  let mantenimiento = Math.round(liquido / 24);
+  let mm2 = Math.floor(mantenimiento * 1.5);
+
+  resultado1.innerText = total + "cc/24hr.";
+  resultado2.innerText = mantenimiento + "cc/hr. " + mm2 + "m+m/2 ";
+}
+
+const calculoHasta30 = () => {
   let pesokg = Number(peso.value);
   let liquido = 0;
   let resto = 0;
@@ -48,21 +60,27 @@ const calculo = () => {
     if (pesokg > 20 && pesokg < 30) liquido = resto * 20 + 1500;
 
     if (pesokg == 30) liquido = 1700;
+
+    calculoDetalles(pesokg, liquido);
   } else if (pesokg > 30) {
-    liquido = Math.floor(((pesokg * 4 + 7) / (pesokg + 90)) * 2000);
-    //En este caso se multiplica por 2000, pero luego cambiare dandole la opcion
-    //al medico que pueda elegir entre 1500 o 2000
+    mayor30.style = "display: block";
+    resultado1.innerText = "";
+    resultado2.innerText = "";
   }
-
-  let total = liquido;
-  let mantenimiento = Math.round(liquido / 24);
-  let mm2 = Math.floor(mantenimiento * 1.5);
-
-  resultado1.innerText = total + "cc/24hr.";
-  resultado2.innerText = mantenimiento +  "cc/hr. " + mm2 + "m+m/2 ";
 };
 
-calcular.addEventListener("click", calculo);
-peso.addEventListener("keypress", (e)=>{
-    if(e.key == 'Enter') calculo();
-})
+const calculoMayor30 = (e)=>{
+  let pesokg = Number(peso.value);
+  let factor = Number(e.target.id);
+  let liquido = Math.floor(((pesokg * 4 + 7) / (pesokg + 90)) * factor);
+  calculoDetalles(pesokg, liquido);
+}
+
+calcular.addEventListener("click", calculoHasta30);
+
+peso.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") calculoHasta30();
+});
+
+op1500.addEventListener("click", calculoMayor30);
+op2000.addEventListener("click", calculoMayor30);
